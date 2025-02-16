0686c6e4af6ce2a70030369113211979721d9d9d
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-02-16 18:51:32 -0300

    Classes Microondas e ProgramaAquecimento

---
+++ MicroondasApp/Models/Microondas.cs
@@ -0,0 +1,59 @@
+﻿// Models/Microondas.cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroondasApp.Models
+{
+    public class Microondas
+    {
+        [Required]
+        [Range(1, 120, ErrorMessage = "O tempo deve estar entre 1 segundo e 2 minutos.")]
+        public int TempoSegundos { get; set; } = 30;
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "A potência deve estar entre 1 e 10.")]
+        public int Potencia { get; set; } = 10;
+
+        public string Aquecimento { get; set; }
+        public bool EmExecucao { get; set; }
+        public bool EmPausa { get; set; }
+        public List<ProgramaAquecimento> Programas { get; set; } = new List<ProgramaAquecimento>();
+
+        public void IniciarAquecimento()
+        {
+            if (TempoSegundos < 1 || TempoSegundos > 120)
+                throw new ArgumentException("Tempo inválido");
+
+            EmExecucao = true;
+            EmPausa = false;
+            Aquecimento = GerarStringAquecimento();
+        }
+
+        public void PausarOuCancelar()
+        {
+            if (EmExecucao)
+            {
+                EmPausa = !EmPausa;
+                if (!EmPausa) EmExecucao = false;
+            }
+            else
+            {
+                TempoSegundos = 30;
+                Potencia = 10;
+                Aquecimento = "";
+            }
+        }
+
+        public void AdicionarTempo()
+        {
+            if (EmExecucao)
+                TempoSegundos = Math.Min(TempoSegundos + 30, 120);
+        }
+
+        private string GerarStringAquecimento()
+        {
+            return new string('.', Potencia) + " Aquecimento concluído";
+        }
+    }
+}
---
+++ MicroondasApp/Models/ProgramaAquecimento.cs
@@ -0,0 +1,13 @@
+﻿// Models/ProgramaAquecimento.cs
+namespace MicroondasApp.Models
+{
+    public class ProgramaAquecimento
+    {
+        public string Nome { get; set; }
+        public string Alimento { get; set; }
+        public int Tempo { get; set; }
+        public int Potencia { get; set; }
+        public string CaracterAquecimento { get; set; }
+        public string Instrucoes { get; set; }
+    }
+}
