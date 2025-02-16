ad587842a29a0a6c0b682e1dd1cbc3d587bfe7c1
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-02-16 18:45:28 -0300

    Add project files.

---
+++ MicroondasApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroondasApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
